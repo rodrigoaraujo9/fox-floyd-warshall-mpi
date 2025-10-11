106c86e8f1e246d677b57332e83e3414ed4d1c3e
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2025-10-11 12:54:53 +0100

    slow apsp implemented

--- main.c
+++ main.c
@@ -48,7 +48,7 @@ void print_matrix(int **matrix, int size, const char *label) {
   }
 }
 
-int read_matrix_size(FILE *file, int *size) {
+int read_size(FILE *file, int *size) {
   if (fscanf(file, "%d", size) != 1) {
     fprintf(stderr, "could not read size\n");
     return 0;
@@ -56,7 +56,7 @@ int read_matrix_size(FILE *file, int *size) {
   return 1;
 }
 
-int read_matrix_data(FILE *file, int **matrix, int size) {
+int read_matrix(FILE *file, int **matrix, int size) {
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
       if (fscanf(file, "%d", &matrix[i][j]) != 1) {
@@ -78,7 +78,7 @@ int read_matrix_data(FILE *file, int **matrix, int size) {
   return 1;
 }
 
-int **load_matrix_from_file(const char *filename, int *out_size) {
+int **parse_matrix_file(const char *filename, int *out_size) {
   FILE *file = fopen(filename, "r");
   if (file == NULL) {
     perror("error opening file");
@@ -86,7 +86,7 @@ int **load_matrix_from_file(const char *filename, int *out_size) {
   }
 
   int size;
-  if (!read_matrix_size(file, &size)) {
+  if (!read_size(file, &size)) {
     fclose(file);
     return NULL;
   }
@@ -98,7 +98,7 @@ int **load_matrix_from_file(const char *filename, int *out_size) {
     return NULL;
   }
 
-  if (!read_matrix_data(file, matrix, size)) {
+  if (!read_matrix(file, matrix, size)) {
     free_matrix(matrix, size);
     fclose(file);
     return NULL;
@@ -140,30 +140,55 @@ int **special_matrix_mul(int **a, int a_size, int **b, int b_size) {
   return c;
 }
 
+int **copy_matrix(int **m, int size) {
+  int **copy = allocate_matrix(size);
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      copy[i][j] = m[i][j];
+    }
+  }
+  return copy;
+}
+
+int **slow_apsp(int **w, int size) {
+  int k, **d_next;
+
+  int **d = copy_matrix(w, size);
+
+  // from d(2) to d(n-1)
+  for (k = 2; k < size; k++) {
+    d_next = special_matrix_mul(d, size, w, size);
+    if (d_next == NULL) {
+      free_matrix(d, size);
+      fprintf(stderr, "special_matrix_mul failed at k=%d\n", k);
+      return NULL;
+    }
+    free_matrix(d, size);
+    d = d_next;
+  }
+  return d;
+}
+
 int main() {
-  int size1, size2;
-  int **m1 = load_matrix_from_file("matrix_examples/input5", &size1);
-  int **m2 = load_matrix_from_file("matrix_examples/input5_2", &size2);
-  if (m1 == NULL || m2 == NULL) {
+  int size;
+  int **w = parse_matrix_file("matrix_examples/input5", &size);
+  if (w == NULL) {
     return 1;
   }
 
-  printf("size of matrix 1: %d\n", size1);
-  printf("size of matrix 2: %d\n", size2);
+  printf("size of matrix: %d\n", size);
 
-  print_matrix(m1, size1, "matrix 1");
-  print_matrix(m2, size2, "matrix 2");
-  int **op = special_matrix_mul(m1, size1, m2, size2);
-  if (op == NULL) {
-    fprintf(stderr, "matrix multiplication failed\n");
-    free_matrix(m1, size1);
+  print_matrix(w, size, "w");
+  int **d = slow_apsp(w, size);
+  if (d == NULL) {
+    fprintf(stderr, "apsp failed\n");
+    free_matrix(w, size);
     return 1;
   }
 
-  print_matrix(op, size1, "mul matrix");
+  print_matrix(d, size, "final d");
 
-  free_matrix(m1, size1);
-  free_matrix(m2, size2);
-  free_matrix(op, size1);
+  free_matrix(w, size);
+  free_matrix(d, size);
   return 0;
 }
