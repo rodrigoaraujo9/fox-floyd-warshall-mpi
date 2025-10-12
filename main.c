2338c2d80597f6931f46c47b148b7d55adeba300
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2025-10-13 00:51:07 +0100

    all algorithms working

--- main.c
+++ main.c
@@ -9,11 +9,14 @@ typedef struct {
 
 Matrix read_matrix_from_file(char* file_path);
 void print_matrix(Matrix matrix, char *title);
-void destroy_matrix(Matrix matrix);
-int special_matrix_mul(Matrix a, Matrix b, Matrix *buf);
 void copy_matrix(Matrix matrix_to_copy, Matrix* buf);
+void destroy_matrix(Matrix matrix);
+
+int special_matrix_mul(Matrix a, Matrix b, Matrix *buf);
+
 int slow_apsp(Matrix w, Matrix *buf);
 int repeated_squaring_apsp(Matrix w, Matrix *buf);
+int floyd_warshall_apsp(Matrix w, Matrix *buf);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -30,14 +33,21 @@ int main(int argc, char **argv) {
     print_matrix(matrix_slow_apsp, "Slow APSP");
 
     Matrix matrix_rs_apsp;
-    if (slow_apsp(matrix, &matrix_rs_apsp) != 0) {
+    if (repeated_squaring_apsp(matrix, &matrix_rs_apsp) != 0) {
         fprintf(stderr, "Failed to apsp!\n");
     }
     print_matrix(matrix_rs_apsp, "Repeated Squaring APSP");
 
+    Matrix matrix_fw_apsp;
+    if (floyd_warshall_apsp(matrix, &matrix_fw_apsp) != 0) {
+        fprintf(stderr, "Failed to apsp!\n");
+    }
+    print_matrix(matrix_fw_apsp, "Floyd Warshall APSP");
+
     destroy_matrix(matrix);
     destroy_matrix(matrix_slow_apsp);
     destroy_matrix(matrix_rs_apsp);
+    destroy_matrix(matrix_fw_apsp);
     return 0;
 }
 
@@ -115,46 +125,43 @@ int special_matrix_mul(Matrix a, Matrix b, Matrix *buf) {
     return 1;
   }
 
-  Matrix c = (Matrix) {(int**) malloc(sizeof(int*) * a.n), a.n};
+  *buf = (Matrix) {(int**) malloc(sizeof(int*) * a.n), a.n};
   int help;
 
-  for (int i = 0; i < c.n; i++) {
-      c.values[i] = (int*) malloc(sizeof(int) * c.n);
-      for (int j = 0; j < c.n; j++) {
-          c.values[i][j] = INT_MAX;
-          for (int k = 0; k < c.n; k++) {
+  for (int i = 0; i < (*buf).n; i++) {
+      (*buf).values[i] = (int*) malloc(sizeof(int) * (*buf).n);
+      for (int j = 0; j < (*buf).n; j++) {
+          (*buf).values[i][j] = INT_MAX;
+          for (int k = 0; k < (*buf).n; k++) {
               if (a.values[i][k] != INT_MAX && b.values[k][j] != INT_MAX) {
                   help = (a.values[i][k] + b.values[k][j]);
-                  if (c.values[i][j] > help) {
-                      c.values[i][j] = help;
+                  if ((*buf).values[i][j] > help) {
+                      (*buf).values[i][j] = help;
                   }
               }
           }
       }
   }
 
-  *buf = c;
-
   return 0;
 }
 
 // 3 implementation sequential
 int slow_apsp(Matrix w, Matrix *buf) {
-  Matrix d, d_next;
+  Matrix d_next;
 
-  copy_matrix(w, &d);
+  copy_matrix(w, buf);
 
   // from d(2) to d(n-1)
   for (int k = 2; k < w.n - 1; k++) {
-    special_matrix_mul(d, w, &d_next);
+    special_matrix_mul(*buf, w, &d_next);
     if (d_next.values == NULL) {
       fprintf(stderr, "special_matrix_mul failed at k=%d\n", k);
       return 1;
     }
-    destroy_matrix(d);
-    d = d_next;
+    destroy_matrix(*buf);
+    *buf = d_next;
   }
-  *buf = d;
   return 0;
 }
 
@@ -162,10 +169,10 @@ int slow_apsp(Matrix w, Matrix *buf) {
 // 3.1 implementation sequential
 int repeated_squaring_apsp(Matrix w, Matrix *buf) {
   int m;
-  Matrix d_2m, d_m;
-  copy_matrix(w, &d_m);
+  Matrix d_2m;
+  copy_matrix(w, buf);
 
-  if (d_m.values == NULL) {
+  if ((*buf).values == NULL) {
     fprintf(stderr, "Failed to copy initial matrix\n");
     return 1;
   }
@@ -173,17 +180,41 @@ int repeated_squaring_apsp(Matrix w, Matrix *buf) {
   m = 1;
 
   while (m < w.n - 1) {
-    special_matrix_mul(d_m, d_m, &d_2m);
+    special_matrix_mul((*buf), (*buf), &d_2m);
     if (d_2m.values == NULL) {
-      destroy_matrix(d_m);
+      destroy_matrix(*buf);
       fprintf(stderr, "special_matrix_mul failed at m=%d\n", m);
       return 1;
     }
 
     m *= 2;
-    destroy_matrix(d_m);
-    d_m = d_2m;
+    destroy_matrix(*buf);
+    (*buf) = d_2m;
   }
-  *buf = d_m;
+  return 0;
+}
+
+int floyd_warshall_apsp(Matrix w, Matrix *buf) {
+  // d(0)=w
+  int new_d;
+  copy_matrix(w, buf);
+
+  if ((*buf).values == NULL) {
+    fprintf(stderr, "Failed to copy initial matrix\n");
+    return 1;
+  }
+
+  for (int k = 0; k < w.n; k++) {
+    for (int i = 0; i < w.n; i++) {
+      for (int j = 0; j < w.n; j++) {
+        if ((*buf).values[i][k] != INT_MAX && (*buf).values[k][j] != INT_MAX) {
+          new_d = ((*buf).values[i][k] + (*buf).values[k][j]);
+          if ((*buf).values[i][j] > new_d) {
+            (*buf).values[i][j] = new_d;
+          }
+        }
+      }
+    }
+  }
   return 0;
 }
