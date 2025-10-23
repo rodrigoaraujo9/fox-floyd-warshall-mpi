ce5833db260010f01ed0b3a89d8120d6fad2b064
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2025-10-23 04:05:56 +0100

    Update blocked_fw.h

--- includes/blocked_fw.h
+++ includes/blocked_fw.h
@@ -10,7 +10,6 @@ int **get_block(int **matrix, int b_row, int b_col, int b, int n);
 int blocked_floyd_warshall_apsp(Matrix w, Matrix *buf, int b);
 int blocked_floyd_warshall_p_apsp(Matrix w, Matrix *buf, int b);
 
-/* Internal helpers exposed for testing if desired */
 void floyd_kernel(int **C, int **A, int **B, int b);
 
 #endif // BLOCKED_FW_H
