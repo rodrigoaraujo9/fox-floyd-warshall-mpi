8177cad784a08edd3fa766a89a057ba74b65d359
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2025-10-24 13:57:05 +0100

    broadcast block to row

--- includes/blocked_fw.h
+++ includes/blocked_fw.h
@@ -7,6 +7,8 @@ void setup_cart(CartInfo *cart, int n);
 
 int **get_block(int **matrix, int b_row, int b_col, int b, int n);
 
+int broadcast_block_to_row(int **block_A, int **new_block, int na, int ma, int step, CartInfo* cart);
+
 int blocked_floyd_warshall_apsp(Matrix w, Matrix *buf, int b);
 int blocked_floyd_warshall_p_apsp(Matrix w, Matrix *buf, int b);
 
