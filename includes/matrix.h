31dc8320d9e12e6a1324d6621734cef9d79099c1
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2025-10-24 22:56:19 +0100

    matrix copy fixed

--- includes/matrix.h
+++ includes/matrix.h
@@ -6,5 +6,6 @@
 void copy_matrix(Matrix src, Matrix *dst);
 void destroy_matrix(Matrix m);
 void assert_apsp(Matrix a, Matrix b, char *title);
+int allocate_matrix(int **buf, int n);
 
 #endif // MATRIX_H
